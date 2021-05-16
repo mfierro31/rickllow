6cd0eea1610dbcd4ffae29699a11bdf569e2d296
Mike Fierro <mfierro31@yahoo.com>
2021-05-16 00:41:15 -0700

    added categories to Home component

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,10 @@ import React from 'react';
 import SearchForm from './SearchForm';
 import './Home.css';
 import banner from './home_banner.jpeg';
+import earth from './earth.png';
+import citadel from './citadel_of_ricks.png';
+import cromulons from './Cromulon_Dimension.png';
+import greasyGrandmaWorld from './greasy_grandma_world.jpeg';
 
 const Home = () => {
   return (
@@ -11,6 +15,23 @@ const Home = () => {
         <h1>Tired of your place in the universe?</h1>
         <SearchForm classOrNoClass="Home-search-form" />
         <h4>We can help you change that.</h4>
+      </div>
+      <h2 className="mt-3">Search by category</h2>   
+      <div>
+        <h4>Planets</h4>
+        <img src={earth} className="img-fluid" alt="earth" />
+      </div>
+      <div>
+        <h4>Space Stations</h4>
+        <img src={citadel} className="img-fluid" alt="citadel of ricks" />
+      </div>
+      <div>
+        <h4>Dimensions</h4>
+        <img src={cromulons} className="img-fluid" alt="cromulon dimension" />
+      </div>
+      <div>
+        <h4>Everything else</h4>
+        <img src={greasyGrandmaWorld} className="img-fluid" alt="greasy grandma world" />
       </div>
     </div>
   );
--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './SearchForm.css';
 
 const SearchForm = ({ classOrNoClass }) => {
   return (
     <form className={classOrNoClass ? classOrNoClass : ""}>
-      <input type="text" className="form-control" placeholder="enter a location name" />
+      <div className="input-container">
+        <input type="text" className="form-control" placeholder="Enter a location name" />
+        <button><i className="fas fa-search text-primary"></i></button>
+      </div>
     </form>
   );
 }
