1dae370d781c9a391bdb95c38e079561f1b75e66
Mike Fierro <mfierro31@yahoo.com>
2021-05-22 14:56:30 -0700

    added save button (heart icons) on ListingCard component

--- src/ListingCard.js
+++ src/ListingCard.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './ListingCard.css';
 
 const ListingCard = ({ id, name, cost, image }) => {
   return (
-    <div className="card rounded shadow">
+    <div className="ListingCard card rounded shadow">
       <Link to={`/listings/${id}`}>
-        <img src={`/images/${image}`} className="card-img-top img-fluid" alt={name} />
+        <div className="ListingCard-img-container">
+          <img src={`/images/${image}`} className="card-img-top img-fluid" alt={name} />
+          <i className="far fa-heart ListingCard-heart"></i>
+        </div>
       </Link>
       <div className="card-body rounded-bottom">
         <h5 className="card-title">{name}</h5>
