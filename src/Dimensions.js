fa530ad556c7f07e774785a48a02be22dfe1db6e
Mike Fierro <mfierro31@yahoo.com>
2021-06-12 23:02:30 -0700

    corrected SearchForm button positionon lg and up screens, cleaned up messy code in Dimensions

--- src/Dimensions.js
+++ src/Dimensions.js
@@ -5,9 +5,10 @@ import locations from './myLocations';
 import ListingCard from './ListingCard';
 
 const Dimensions = () => {
-  const dimensionsWithDuplicates = locations.map(l => l.dimension);
-  const dimensionsNoDuplicates = new Set(dimensionsWithDuplicates);
-  const dimensionsArr = Array.from(dimensionsNoDuplicates).map((d, i) => {
+  const dimensionsSet = new Set();
+  locations.forEach(l => dimensionsSet.add(l.dimension));
+
+  const dimensionsArr = Array.from(dimensionsSet).map((d, i) => {
     const listings = locations.filter(l => l.dimension === d);
     return {
       id: i,
@@ -22,7 +23,7 @@ const Dimensions = () => {
       <div className="row justify-content-center">
         {dimensionsArr.map(d => (
           <div key={d.id} className="col-12">
-            <h3>{d.name}({d.listings.length} locations)</h3>
+            <h3>{d.name}({d.listings.length} location{`${d.listings.length > 1 ? 's' : ''}`})</h3>
             <div className="row justify-content-center">
               {d.listings.map(l => (
                 <div key={l.id} className="col-12 col-md-6 col-lg-4 col-xxl-3 mb-4">
